1c04f17ae541a97c184055eb6de6255fb9b0a9af
D.Soldatov <dmitriysoldatov1998@gmail.com>
2025-06-24 16:44:30 +0500

    init

---
+++ src/main/java/algo/trading/tgalerter/TgAlerterApp.java
@@ -0,0 +1,13 @@
+package algo.trading.tgalerter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Start spring application. */
+@SpringBootApplication
+public class TgAlerterApp {
+  /** Main method for start application. */
+  public static void main(String[] args) {
+    SpringApplication.run(TgAlerterApp.class, args);
+  }
+}
